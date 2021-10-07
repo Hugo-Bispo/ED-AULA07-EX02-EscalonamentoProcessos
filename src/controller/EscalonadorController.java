ecd2dfb25b873636c53ddd780161d6eb53367241
Gugo <Gugo@PC-HG01>
2021-10-07 00:13:50 -0300

    V 1.0

--- src/controller/EscalonadorController.java
+++ src/controller/EscalonadorController.java
@@ -1,5 +1,23 @@
 package controller;
 
+import br.edu.fateczl.hugobispo.filastring.Fila;
+
 public class EscalonadorController {
+
+	public void Escalonador(Fila fila) {
+		String name = "";
+		if(fila.size() == 0) {
+			System.out.println("Tamanho fila Invalida");
+		}
+		try {
+			name = fila.remove();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String num [] = name.split(";");
+		int qtdvezes = Integer.parseInt(num[1]);
+		System.out.println(qtdvezes);
+	}
 
 }
